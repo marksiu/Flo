ec22343d5191eb56e52eb5a174bd4f1983ef9753
Mark Siu <mark.siu@gtomato.com>
2016-10-19 19:51:41 +0800

    Added the label to graphics view chart

--- Flo/Flo/ViewController.swift
+++ Flo/Flo/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var counterView: CounterView!
     @IBOutlet weak var counterLabel: UILabel!
     
+    //Label outlets
+    @IBOutlet weak var averageWaterDrunk: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         counterLabel.text = String(counterView.counter)
@@ -51,8 +55,58 @@ class ViewController: UIViewController {
                               duration: 1.0,
                               options: [UIViewAnimationOptions.transitionFlipFromRight, UIViewAnimationOptions.showHideTransitionViews],
                               completion: nil)
+            setupGraphDisplay()
         }
         isGraphViewShowing = !isGraphViewShowing
+    }
+    
+    func setupGraphDisplay() {
+        
+        //Use 7 days for graph - can use any number,
+        //but labels and sample data are set up for 7 days
+        let noOfDays:Int = 7
+        
+        //1 - replace last day with today's actual data
+        graphView.graphPoints[graphView.graphPoints.count-1] = counterView.counter
+        
+        //2 - indicate that the graph needs to be redrawn
+        graphView.setNeedsDisplay()
+        
+        maxLabel.text = "\(graphView.graphPoints.max()!)"
+        
+        //3 - calculate average from graphPoints
+        let average = graphView.graphPoints.reduce(0, +)
+            / graphView.graphPoints.count
+        averageWaterDrunk.text = "\(average)"
+        
+        //set up labels
+        //day of week labels are set up in storyboard with tags
+        //today is last day of the array need to go backwards
+        
+        //4 - get today's day number
+        let dateFormatter = DateFormatter()
+        let calendar = NSCalendar.current
+        let componentOptions:NSCalendar.Unit = NSCalendar.Unit.weekday
+        let components = calendar.dateComponents([Calendar.Component.weekday], from: NSDate() as Date)
+        var weekday = components.weekday
+        
+        // default index 0 is Sat, Sun, ..., Fri
+        let days = ["S", "S", "M", "T", "W", "T", "F"]
+        
+        //5 - set up the day name labels with correct day
+        for i in (1...days.count).reversed() {
+            if let labelView = graphView.viewWithTag(i) as? UILabel {
+                if weekday == 7 {
+                    weekday = 0
+                }
+                labelView.text = days[weekday!]
+                weekday = weekday! - 1
+                if weekday! < 0 {
+                    // reset the counter, so that it goes to the last index of "F"
+                    weekday = days.count - 1
+                }
+            }
+        }
     }
 
 }
